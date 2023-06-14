30f1897e28fd1c31c3a1b2e6217712a7f6b88ab6
José Carlos Montañez <josecarlos.aragon@47deg.com>
2023-06-14 10:37:09 +0200

    Updated config reading

--- src/configHelpers.py
+++ src/configHelpers.py
@@ -1,13 +1,31 @@
 import json
+from collections import namedtuple
+from json import JSONEncoder
 
-def readConfig():
+class Instrument:
+    def __init__(self, name, restrictLevel, publishName):
+        self.name = name
+        self.restrictLevel = restrictLevel
+        self.publishName = publishName
+
+class InstrumentEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+def instrumentDecoder(instrumentDict):
+    return namedtuple('X', instrumentDict.keys())(*instrumentDict.values())
+
+def readConfig(toClass = True):
     with open("application.conf", 'r') as configFile:
-        return json.load(configFile)
+        if toClass:
+            return json.load(configFile, object_hook=instrumentDecoder)
+        else:
+            return json.load(configFile)
 
 def getSearchElements(config):
     res = []
     for row in config:
-        res.append(row['name'])
+        res.append(row.name)
     return res
 
 def getElementsWithURL(config, urls):
@@ -16,5 +34,5 @@ def getElementsWithURL(config, urls):
     return config
 
 if __name__ == '__main__':
-    config = readConfig()
+    config = readConfig(True)
     print(getSearchElements(config))
