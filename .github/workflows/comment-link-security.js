b301a17f4b3eab4ce0c325f43074cc18fcca5306
José Carlos Montañez <josecarlos.aragon@47deg.com>
2023-07-05 10:51:31 +0200

    added new comment

--- .github/workflows/comment-link-security.js
+++ .github/workflows/comment-link-security.js
@@ -3,16 +3,18 @@ module.exports = ({github, context}) => {
 }
 
 function manageComments(github, context) {
+    prNumber = context.issue.number
+    console.log(prNumber)
     url = github.repositoryUrl +
         "/security/code-scanning?query=pr%3A"+
-        context.issue.numer +
+        prNumber +
         "+tool%3AFormalm+is%3Aopen"
     const body = "FormaLM has performed a code analysis" +
         "and generated a report." +
-        "Please check the [Security report]("+url+")"+
-        "for this."
+        "Please check the [Security report]("+url+") "+
+        "for more information."
     github.rest.issues.createComment({
-      issue_number: context.issue.number,
+      issue_number: prNumber,
       owner: context.repo.owner,
       repo: context.repo.repo,
       body: body
